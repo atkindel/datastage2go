2af4fa3e48e02628e4ab8830d0b23a454e0bac3c
Alex Kindel <atkindel@stanford.edu>
2016-01-20 13:28:49 -0800

    Clean up and deactivate old fixes, add BSD licenses

--- scripts/database_config.sh
+++ scripts/database_config.sh
@@ -1,3 +1,14 @@
+# Copyright (c) 2014, Stanford University
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+# 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+# 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
 COLOR="\033[1;35m"
 COLOR_RST="\033[0m"
 
@@ -25,12 +36,13 @@ echo -e "${COLOR}---Cloning OpenEdX ETL software...---${COLOR_RST}"
   # cd forum_etl
   # python setup.py install
 
+## The code below disables partitioning on the primary event log table. Use with caution.
 
-echo -e "${COLOR}---Excluding default partitioning...---${COLOR_RST}"
+#echo -e "${COLOR}---Excluding default partitioning...---${COLOR_RST}"
 
-  sudo sed -i "s/tableName == 'EdxTrackEvent'/tableName == 'NULL_TABLE'/g" /home/dataman/Code/json_to_relation/json_to_relation/edxTrackLogJSONParser.py
-  sudo sed -i '163,193d' scripts/createEmptyEdxDbs.sql
-  sudo sed -i '162s/$/;/' scripts/createEmptyEdxDbs.sql
+  # sudo sed -i "s/tableName == 'EdxTrackEvent'/tableName == 'NULL_TABLE'/g" /home/dataman/Code/json_to_relation/json_to_relation/edxTrackLogJSONParser.py
+  # sudo sed -i '163,193d' scripts/createEmptyEdxDbs.sql
+  # sudo sed -i '162s/$/;/' scripts/createEmptyEdxDbs.sql
 
 
 # Create data export directories
@@ -54,8 +66,6 @@ echo -e "${COLOR}---Setup MySQL authentication...---${COLOR_RST}"
 
   echo "[client]" >> /root/.my.cnf
   echo "password=root" >> /root/.my.cnf
-  # sudo rm /etc/mysql/my.cnf
-  # sudo cp /home/vagrant/sync/my.cnf /etc/mysql/
 
   cd /home/dataman/Code/json_to_relation
   sudo sed -i "s/--login-path=root//g" scripts/createEmptyEdxDbs.sh
